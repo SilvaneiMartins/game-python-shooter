b89d0eba2123db3d4b9910501ebb0775e62881cd
Silvanei  Martins <silvaneimartins@Silvaneis-MacBook-Air.local>
2024-02-07 22:18:22 -0400

    feat: explosions

--- shooter.py
+++ shooter.py
@@ -238,6 +238,17 @@ class Grenade(pygame.sprite.Sprite):
 		self.rect.x += dx
 		self.rect.y += dy
 
+# Classe de explosão
+class Explosion(pygame.sprite.Sprite):
+	def __init__(self, x, y):
+		pygame.sprite.Sprite.__init__(self)
+		self.images = []
+		for num in range(1,6):
+		    img = pygame.image.load(f'img/explosion/exp{num}.png').convert_alpha()
+            img = pygame.transform.scale(img, (int(img.get_width() * scale), int(img.get_height() * scale)))
+		self.image = grenade_img
+		self.rect = self.image.get_rect()
+		self.rect.center = (x, y)
 
 # Criar grupos de sprites
 bullet_group = pygame.sprite.Group()
